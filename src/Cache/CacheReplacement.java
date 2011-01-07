069240fea08daef6505d21b5c238045043125429
supertri <supertri@cs.ucsc.edu>
2011-01-07 14:58:44 -0800

    Added LRU tiebreaking to the cache replacement algorithm

--- src/Cache/CacheReplacement.java
+++ src/Cache/CacheReplacement.java
@@ -7,8 +7,8 @@ public class CacheReplacement {
 	public static enum Policy{LRU,BUGS,CHANGES,AUTHORS};
 	static final CacheReplacement.Policy REPDEFAULT = CacheReplacement.Policy.LRU;
 
-
 	protected Comparator<CacheItem> compareFunc;
+	protected ComparatorLRU tiebreaker;
 	
 	public CacheReplacement(Policy p){
 		switch (p){
@@ -20,7 +20,14 @@ public class CacheReplacement {
 	}
 
 	public CacheItem minimum(CacheItem o1, CacheItem o2){
-		if (compareFunc.compare(o1, o2) <0 )
+		int comparison = compareFunc.compare(o1, o2);		
+		if (comparison == 0){
+			if (tiebreaker.compare(o1, o2) < 0)
+				return o1;
+			else
+				return o2;
+		}
+		else if (comparison < 0 )
 			return o1;
 		else
 			return o2;
