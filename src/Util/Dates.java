af05e435ff89f39a13d0e0bf7478d88427e15f9d
supertri <supertri@cs.ucsc.edu>
2011-01-10 12:26:46 -0800

    Added more details to README

---
+++ src/Util/Dates.java
@@ -0,0 +1,11 @@
+package Util;
+
+public class Dates {
+
+	// XXX: what is the unit of return value
+	static int TimeDifference(String date1, String date2){
+		// parse the two dates to figure out how many hours (?) are between them
+	}
+	
+	
+}
