06f6c6e9f13dcc9feab38c83c6cacc8fea8977a6
supertri <supertri@cs.ucsc.edu>
2011-01-13 09:23:47 -0800

    added some more comments to Cache.java

--- src/Cache/Cache.java
+++ src/Cache/Cache.java
@@ -10,20 +10,20 @@ public class Cache {
     /**
      *  Invariant: cacheTable.size() <= size;
      */
-    
+
     /**
      * Fields 
      */
     private final int maxsize; 
     private int size = 0; // current size of cache
-    
+
     // keeps every cacheitem that was ever in the cache
     private Hashtable<Integer, CacheItem> cacheTable = new Hashtable<Integer, CacheItem>();
-    
+
     private CacheReplacement policy;
     String startDate;
     int repID;
-    
+
     // counter, used to decide which cacheitem is LRU
     private int time = 0;
 
@@ -37,19 +37,19 @@ public class Cache {
     /**
      * Methods
      */
-    
+
     /**
      * Compares the current cache size with the maximum allowable
      * @return whether the cache is at full capacity
      */
     public boolean isFull() {
-        int currentCacheSize = size;
-        assert (currentCacheSize <= maxsize);
-        return currentCacheSize == maxsize;
+        assert (size <= maxsize);
+        return size == maxsize;
     }
 
     /**
      * Loads an item into the cache, removing an item if full
+     * The only method where size is increased.
      * @param cacheItem
      */
     // XXX: load a chunk at a time??
@@ -61,35 +61,56 @@ public class Cache {
             cacheTable.put(entityId, cacheItem);
         size++;
     }
-    
+
+    /**
+     * Removes an item from the cache by switching the 'inCache' boolean to false.
+     * The only method that decreases size.
+     * @param fileid 
+     */
     public void remove(int fileid) {
         cacheTable.get(fileid).removeFromCache();
         size--;
     }
 
-
+    /**
+     * Called on cache hit and cache miss.
+     * Adds an item into the cache, if not already there.
+     * If already there, updates the cache entry.
+     * @param eid -- entityid
+     * @param cid -- commitid
+     * @param cdate -- commit date
+     * @param reason -- reason for adding to the cache
+     */
     public void add(int eid, int cid, String cdate, CacheReason reason) {
-        if (cacheTable.containsKey(eid)){
+        if (cacheTable.containsKey(eid)){ // either in cache or was bumped out
             CacheItem ci = cacheTable.get(eid);
             if (!ci.isInCache()){
                 load(ci);
             }
-            ci.update(cid, cdate, startDate);
-        } else {
+            ci.update(cid, cdate, startDate); // updates inCache status
+        } else { // need to create a new CacheItem
             load(new CacheItem(eid, cid, cdate, reason, this));
         }
     }
 
-    public void add(ArrayList<Integer> eids, int cid, String cdate,
-            CacheReason reas) {
+    /**
+     * Wrapper for add to add in bulk
+     * @param eids -- entityid list
+     * @param cid -- commitid
+     * @param cdate -- commit date
+     * @param reason -- reason for adding to the cache
+     */
+    public void add(ArrayList<Integer> eids, int cid, String cdate, CacheReason reas) {
         for (int eid : eids)
             add(eid, cid, cdate, reas);
     }
 
 
-    // figures out what to remove with cache replacement policy
-    // iterates through the map and find the minimum element (given the cache
-    // replacement policy) then removes that element
+    /**
+     *  Figures out what to remove with cache replacement policy.
+     *   iterates through the map and find the minimum element (given the cache
+     *   replacement policy) then removes that element
+     */
     // TODO: keep cache always sorted using cache replacement policy
     public void bumpOutItem() {
         int entityId = getMinimum();
@@ -101,7 +122,7 @@ public class Cache {
 
         for (CacheItem c : cacheTable.values()) {
             if (!c.isInCache()) continue;
-            
+
             if (min == null)
                 min = c;
             else
@@ -110,24 +131,20 @@ public class Cache {
         return min.getEntityId();
     }
 
+    /**
+     * Wrapper for bumping out multiple items at once.
+     * @param numItems
+     */
     public void bumpOutItem(int numItems) {
         for (int i = 0; i < numItems; ++i)
             bumpOutItem();
     }
     
+
     /**
      * Getters
      */
 
-    
-    public CacheItem getCacheItem(int entityId) {
-        CacheItem ci = cacheTable.get(entityId);
-        if (ci == null)
-            return null;
-        if (!ci.isInCache())
-            return null;
-        return cacheTable.get(entityId);
-    }
 
     public CacheReplacement.Policy getPolicy() {
         return policy.currentPolicy;
@@ -144,7 +161,7 @@ public class Cache {
     public int getCacheSize() {
         return size;
     }
-    
+
     /**
      * Checks the cache table for whether a particular entity is in the cache
      * @param eid -- entity id
@@ -157,12 +174,26 @@ public class Cache {
         else // in cacheTable
             return ci.isInCache();
     }
-    
-    
+
+
     /**
      *  Methods for debugging
      */
+
     
+    /**
+     * Returns either the CacheItem associated with entityId, if in the cache
+     * or null if it is not in the cache.
+     */
+    public CacheItem getCacheItem(int entityId) {
+        CacheItem ci = cacheTable.get(entityId);
+        if (ci == null)
+            return null;
+        if (!ci.isInCache())
+            return null;
+        return ci;
+    }
+
     public ArrayList<CacheItem> getCacheItemList() {
         ArrayList<CacheItem> CIList = new ArrayList<CacheItem>();
         Iterator<CacheItem> it = cacheTable.values().iterator();
@@ -176,7 +207,7 @@ public class Cache {
         CacheItem ci = cacheTable.get(fileid);
         return ci.getNumber();
     }
-    
+
     public int getLoadCount(int fileid){
         CacheItem ci = cacheTable.get(fileid);
         return ci.getLoadCount();
