f5c79cbf888c4159d046c3d95cadb59cb870b17f
Xiaoyan Zhu <xyzhu@elephant.cse.ucsc.edu>
2011-03-01 16:14:54 -0800

    use correct loc for a file

--- src/test/java/FixCache/Test/CacheItemTest.java
+++ src/test/java/FixCache/Test/CacheItemTest.java
@@ -41,44 +41,44 @@ public class CacheItemTest {
     @Test
     public void testCacheItemGet() {
         Cache cache = new Cache(5, new CacheReplacement(
-                CacheReplacement.Policy.AUTHORS), "2009-10-20 01:32:19.0","2010-01-01 01:01:01.0", 1);
-        CacheItem ci1 = new CacheItem("e.java", 10, "2009-10-24 14:30:54.0",
+                CacheReplacement.Policy.AUTHORS), "2009-10-20 01:32:19","2010-01-01 01:01:01", 1);
+        CacheItem ci1 = new CacheItem("e.java", 10, "2009-10-24 14:30:54",
                 CacheReason.BugEntity, cache);
         assertEquals(2, ci1.getNumberOfAuthors());
 
         cache = new Cache(5,
                 new CacheReplacement(CacheReplacement.Policy.BUGS),
-                "2009-10-20 01:32:19.0", "2010-01-01 01:01:01.0",1);
-        ci1 = new CacheItem("e.java", 10, "2009-10-24 14:30:54.0",
+                "2009-10-20 01:32:19", "2010-01-01 01:01:01",1);
+        ci1 = new CacheItem("e.java", 10, "2009-10-24 14:30:54",
                 CacheReason.BugEntity, cache);
         assertEquals(3, ci1.getNumberOfBugs());
 
         cache = new Cache(5, new CacheReplacement(
-                CacheReplacement.Policy.CHANGES), "2009-10-20 01:32:19.0", "2010-01-01 01:01:01.0",1);
-        ci1 = new CacheItem("e.java", 10, "2009-10-24 14:30:54.0",
+                CacheReplacement.Policy.CHANGES), "2009-10-20 01:32:19", "2010-01-01 01:01:01",1);
+        ci1 = new CacheItem("e.java", 10, "2009-10-24 14:30:54",
                 CacheReason.BugEntity, cache);
 //        CacheItem ci11 = new CacheItem(1, 1, "2009-10-20 01:32:19.0", CacheReason.BugEntity, cache);
         // assertEquals(3, ci1.getNumberOfChanges());
 
         // XXX fails due to bug in content_loc
         // TODO change back later 8 +> 9
-        assertEquals(8, ci1.getLOC());
+        assertEquals(9, ci1.getLOC());
         //assertEquals(2, ci11.getLOC());
 
         cache = new Cache(5, new CacheReplacement(
-                CacheReplacement.Policy.AUTHORS), "2009-10-20 01:32:19.0", "2010-01-01 01:01:01.0",1);
-        CacheItem ci2 = new CacheItem("a.java", 8, "2009-10-24 07:51:22.0",
+                CacheReplacement.Policy.AUTHORS), "2009-10-20 01:32:19", "2010-01-01 01:01:01",1);
+        CacheItem ci2 = new CacheItem("a.java", 8, "2009-10-24 07:51:22",
                 CacheReason.BugEntity, cache);
         assertEquals(4, ci2.getNumberOfAuthors());
 
         cache = new Cache(5,new CacheReplacement(CacheReplacement.Policy.BUGS),
-                "2009-10-20 01:32:19.0","2010-01-01 01:01:01.0", 1);
-        ci2 = new CacheItem("a.java", 8, "2009-10-24 07:51:22.0",
+                "2009-10-20 01:32:19","2010-01-01 01:01:01", 1);
+        ci2 = new CacheItem("a.java", 8, "2009-10-24 07:51:22",
                 CacheReason.BugEntity, cache);
         assertEquals(2, ci2.getNumberOfBugs());
         cache = new Cache(5, new CacheReplacement(
-                CacheReplacement.Policy.CHANGES), "2009-10-20 01:32:19.0","2010-01-01 01:01:01.0",1);
-        ci2 = new CacheItem("a.java", 8, "2009-10-24 07:51:22.0",
+                CacheReplacement.Policy.CHANGES), "2009-10-20 01:32:19","2010-01-01 01:01:01",1);
+        ci2 = new CacheItem("a.java", 8, "2009-10-24 07:51:22",
                 CacheReason.BugEntity, cache);
         assertEquals(5, ci2.getNumberOfChanges());
 
