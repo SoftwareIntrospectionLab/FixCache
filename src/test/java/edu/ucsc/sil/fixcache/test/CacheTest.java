0cb1591b2775aef0b759b89ba84739c05e6f812f
Chris Lewis <chris@chris.to>
2011-06-15 16:44:48 -0700

    CacheTest passing

--- src/test/java/edu/ucsc/sil/fixcache/test/CacheTest.java
+++ src/test/java/edu/ucsc/sil/fixcache/test/CacheTest.java
@@ -38,23 +38,23 @@ public class CacheTest {
     public void testCacheAdd() {
         Cache cache = new Cache(5, new CacheReplacement(
                 CacheReplacement.Policy.AUTHORS), "2009-10-20 01:32:19.0", "2010-01-01 01:01:01.0",1);
-        cache.add("a.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
+        cache.add("/foo/bar/a.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
         assertEquals(cache.getCacheSize(), 1);
-        cache.add("g.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
+        cache.add("/foo/bar/g.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
         assertEquals(cache.getCacheSize(), 2);
-        cache.add("c.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
+        cache.add("/foo/bar/c.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
         assertEquals(cache.getCacheSize(), 3);
-        cache.add("d.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
+        cache.add("/foo/bar/d.java", 1, "2009-10-20 01:32:19.0", CacheReason.NewEntity);
         assertEquals(cache.getCacheSize(), 4);
-        cache.add("a.java", 2, "2009-10-20 14:37:38.0", CacheReason.ModifiedEntity);
+        cache.add("/foo/bar/a.java", 2, "2009-10-20 14:37:38.0", CacheReason.ModifiedEntity);
         assertEquals(cache.getCacheSize(), 4);
-        cache.add("d.java", 2, "2009-10-20 14:37:38.0", CacheReason.ModifiedEntity);
+        cache.add("/foo/bar/d.java", 2, "2009-10-20 14:37:38.0", CacheReason.ModifiedEntity);
         assertEquals(cache.getCacheSize(), 4);
-        cache.add("g.java", 3, "2009-10-20 14:37:47.0", CacheReason.BugEntity);
+        cache.add("/foo/bar/g.java", 3, "2009-10-20 14:37:47.0", CacheReason.BugEntity);
         assertEquals(cache.getCacheSize(), 4);
-        cache.add("e.java", 3, "2009-10-20 14:37:47.0", CacheReason.BugEntity);
+        cache.add("/foo/bar/e.java", 3, "2009-10-20 14:37:47.0", CacheReason.BugEntity);
         assertTrue(cache.isFull());
-        assertEquals(1, cache.getLoadCount("a.java"));
+        assertEquals(1, cache.getLoadCount("/foo/bar/a.java"));
     }
 
 }
