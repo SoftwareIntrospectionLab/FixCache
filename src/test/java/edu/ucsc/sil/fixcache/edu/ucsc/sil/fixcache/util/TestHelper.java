79f2922799c9e16c5b0ccd993a6a5eff412db40f
Chris Lewis <chris@chris.to>
2011-06-17 12:40:47 -0700

    Readding TestHelper

---
+++ src/test/java/edu/ucsc/sil/fixcache/edu/ucsc/sil/fixcache/util/TestHelper.java
@@ -0,0 +1,71 @@
+package edu.ucsc.sil.fixcache.util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
+import org.dbunit.operation.DatabaseOperation;
+
+import edu.ucsc.sil.fixcache.database.DatabaseManager;
+import edu.ucsc.sil.fixcache.database.SqliteDataTypeFactory;
+
+public class TestHelper {
+
+    public static Connection getJDBCConnection() throws Exception {
+        return DatabaseManager.getTestConnection();
+    }
+
+    public static IDatabaseConnection getDBUnitConnection() throws Exception {
+        IDatabaseConnection conn = new DatabaseConnection(getJDBCConnection());
+
+        return conn;
+    }
+
+    public static IDataSet getDataSet() throws Exception {
+        return new FlatXmlDataSet(new FileInputStream(
+                "fixcache.xml"));
+    }
+
+    protected DatabaseOperation getSetUpOperation() throws Exception {
+        return DatabaseOperation.REFRESH;
+    }
+
+    protected DatabaseOperation getTearDownOperation() throws Exception {
+        return DatabaseOperation.NONE;
+    }
+
+    public static void handleSetUpOperation() throws Exception {
+
+        final IDatabaseConnection conn = getDBUnitConnection();
+        configureConnection(conn);
+
+        final IDataSet data = getDataSet();
+        try {
+            DatabaseOperation.CLEAN_INSERT.execute(conn, data);
+        } finally {
+            // conn.close();
+        }
+    }
+
+    public static void cleanDatabase() throws Exception {
+        final IDatabaseConnection conn = getDBUnitConnection();
+        configureConnection(conn);
+        
+        final IDataSet data = getDataSet();
+        try {
+            DatabaseOperation.DELETE_ALL.execute(conn, data);
+        } finally {
+            // conn.close();
+        }
+    }
+    
+    private static void configureConnection(IDatabaseConnection conn) {
+        DatabaseConfig config = conn.getConfig();
+        config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, 
+            new SqliteDataTypeFactory());
+    }
+}
